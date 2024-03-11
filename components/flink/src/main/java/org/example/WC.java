e2ecfa51f202ba860547f13a500b22c39b3bf2cb
jixie <jixie>
2024-03-11 23:15:36 +0800

    add more nodes in aws

---
+++ components/flink/src/main/java/org/example/WC.java
@@ -0,0 +1,61 @@
+package org.example;
+
+import org.apache.flink.api.common.RuntimeExecutionMode;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+/**
+ * nc -l 30001
+ */
+public class WC {
+    public static void main(String[] args) throws Exception {
+        // 1.准备环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironment();
+        // 设置运行模式
+        env.setRuntimeMode(RuntimeExecutionMode.AUTOMATIC);
+        // 2.加载数据源
+        DataStreamSource<String> elementsSource = env.socketTextStream("127.0.0.1", 30001);
+        // 3.数据转换
+        DataStream<String> flatMap = elementsSource.flatMap(new FlatMapFunction<String, String>() {
+            @Override
+            public void flatMap(String element, Collector<String> out) {
+                String[] wordArr = element.split(",");
+                for (String word : wordArr) {
+                    out.collect(word);
+                }
+            }
+        });
+        DataStream<Tuple2<String, Integer>> counts =
+                // split up the lines in pairs (2-tuples) containing: (word,1)
+                flatMap.flatMap(new Tokenizer())
+                        // group by the tuple field "0" and sum up tuple field "1"
+                        .keyBy(value -> value.f0)
+                        .sum(1);
+
+        // 4.数据输出
+        counts.print();
+        // 5.执行程序
+        env.execute("flink-wc");
+    }
+
+    public static final class Tokenizer
+            implements FlatMapFunction<String, Tuple2<String, Integer>> {
+
+        @Override
+        public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
+            // normalize and split the line
+            String[] tokens = value.toLowerCase().split("\\W+");
+
+            // emit the pairs
+            for (String token : tokens) {
+                if (token.length() > 0) {
+                    out.collect(new Tuple2<>(token, 1));
+                }
+            }
+        }
+    }
+}
